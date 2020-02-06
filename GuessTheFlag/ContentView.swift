282d41640eb4f064280bef2eedf809613b021877
Davron <devrannur@gmail.comn>
2020-02-06 13:09:56 -0600

    Accessibility added.

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -44,6 +44,19 @@ struct Shake: GeometryEffect {
 }
 
 struct ContentView: View {
+    let labels = [
+        "Estonia": "Flag with three horizontal stripes of equal size. Top stripe blue, middle stripe black, bottom stripe white",
+        "France": "Flag with three vertical stripes of equal size. Left stripe blue, middle stripe white, right stripe red",
+        "Germany": "Flag with three horizontal stripes of equal size. Top stripe black, middle stripe red, bottom stripe gold",
+        "Ireland": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe orange",
+        "Italy": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe red",
+        "Nigeria": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe green",
+        "Poland": "Flag with two horizontal stripes of equal size. Top stripe white, bottom stripe red",
+        "Russia": "Flag with three horizontal stripes of equal size. Top stripe white, middle stripe blue, bottom stripe red",
+        "Spain": "Flag with three horizontal stripes. Top thin stripe red, middle thick stripe gold with a crest on the left, bottom thin stripe red",
+        "UK": "Flag with overlapping red and white crosses, both straight and diagonally, on a blue background",
+        "US": "Flag with red and white stripes of equal size, with white stars on a blue background in the top-left corner"
+    ]
     
     @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Monaco", "Nigeria", "Poland", "Russia", "Spain", "Turkey", "Turkmenistan", "UK", "US", "Uzbekistan"].shuffled()
     @State private var correctAnswer = Int.random(in: 0...2)
@@ -53,17 +66,18 @@ struct ContentView: View {
     
     @State private var animationAmount = 0.0
     @State private var attempts: Int = 0
-    @State private var flip = false
-    @State private var wrong = false
+    @State private var didSelectCorrectAnswer = false
+    @State private var opacity = 1.0
     
     var body: some View {
         ZStack {
             LinearGradient(gradient: Gradient(colors: [.blue, .black]), startPoint: .top, endPoint: .bottom).edgesIgnoringSafeArea(.all)
             
             VStack (spacing: 40){
+                Spacer()
                 VStack (spacing: 20) {
                     Text("Tap the flag of..")
-                    .foregroundColor(.white)
+                        .foregroundColor(.white)
                     Text(countries[correctAnswer])
                         .foregroundColor(.white)
                         .font(.largeTitle)
@@ -73,12 +87,14 @@ struct ContentView: View {
                 ForEach(0..<3) { number in
                     Button(action: {
                         //flag was tapped
+                        self.opacity = 0.25
                         self.flagTapped(number)
                         withAnimation {
                             if number == self.correctAnswer {
-                                self.animationAmount += 360
+                                self.animationAmount = 360
                             }
                             else {
+                                self.animationAmount = 0
                                 self.attempts += 1
                             }
                         }
@@ -94,15 +110,17 @@ struct ContentView: View {
                                     .stroke(Color.black, lineWidth: 1)
                                     .shadow(color:.black, radius: 1)
                             )
-                            .rotation3DEffect(.degrees( Double(self.animationAmount)), axis: (x: 0, y: number == self.correctAnswer ? 1 : 0, z: 0 ))
-                            .modifier(Shake(animatableData: CGFloat(self.attempts)))
-
+                            .accessibility(label: Text(self.labels[self.countries[number], default: "Unknown flag"]))
                     }
+                    .opacity(number == self.correctAnswer ? 1 : self.opacity)
+                    .animation(.default)
+                    .rotation3DEffect(.degrees( Double(number == self.correctAnswer ?  self.animationAmount : 0.0)), axis: (x: 0, y: 1, z: 0 ))
+                    .modifier(Shake(animatableData: CGFloat(self.attempts)))
 
                 }
-                Text("Score: \(score)")
-                    .foregroundColor(.white).fontWeight(.heavy)
-                Spacer()
+                    Text("Score: \(score)")
+                        .foregroundColor(.white).fontWeight(.heavy)
+                    Spacer()
             }
         }
         .alert(isPresented: $showingScore) {
@@ -116,11 +134,11 @@ struct ContentView: View {
         if number == correctAnswer {
             scoreTitle = "Correct"
             score += 10
-            flip = true
+            didSelectCorrectAnswer = true
         }
         else {
             scoreTitle = "Wrong! That is the flag of \(countries[number])"
-            wrong = true
+            didSelectCorrectAnswer = false
         }
         showingScore = true
     }
@@ -128,8 +146,8 @@ struct ContentView: View {
     func askQuestion() {
         countries.shuffle()
         correctAnswer = Int.random(in: 0...2)
-        flip = false
-        wrong = false
+        animationAmount = 0.0
+        opacity = 1.0
     }
 }
 
